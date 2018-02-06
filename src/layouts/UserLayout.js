4742bb0cfcd44ae20ee70fd275e2dde3b2794f28
baizongjie <baizongjie@qq.com>
2018-02-06 17:12:29 +0800

    init

---
+++ src/layouts/UserLayout.js
@@ -0,0 +1,59 @@
+/**
+ * 登录页
+ */
+import React from 'react';
+import { Redirect, Switch, Route } from 'dva/router';
+import DocumentTitle from 'react-document-title';
+import { Icon } from 'antd';
+import GlobalFooter from '../components/GlobalFooter';
+import styles from './UserLayout.less';
+import logo from '../assets/logo.jpeg';
+import { getRoutes } from '../utils/utils';
+
+const links = [];
+
+const copyright = <div>Copyright <Icon type="copyright" /> 2018 交通银行股份有限公司出品</div>;
+
+class UserLayout extends React.PureComponent {
+  getPageTitle() {
+    const { routerData, location } = this.props;
+    const { pathname } = location;
+    let title = '交通银行资产证券化平台';
+    if (routerData[pathname] && routerData[pathname].name) {
+      title = `${routerData[pathname].name} - ${title}`;
+    }
+    return title;
+  }
+  render() {
+    const { routerData, match } = this.props;
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.top}>
+              <div className={styles.header}>
+                <img alt="logo" className={styles.logo} src={logo} />
+              </div>
+            </div>
+            <Switch>
+              {getRoutes(match.path, routerData).map(item =>
+                (
+                  <Route
+                    key={item.key}
+                    path={item.path}
+                    component={item.component}
+                    exact={item.exact}
+                  />
+                )
+              )}
+              <Redirect exact from="/user" to="/user/login" />
+            </Switch>
+          </div>
+          <GlobalFooter links={links} copyright={copyright} />
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default UserLayout;
