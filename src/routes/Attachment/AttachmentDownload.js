0c225543463e2a48fd65388ecc66e998a3dd19c8
wangna420 <wangna420@gmail.com>
2018-03-22 10:47:13 +0800

    add AttachmentUpload.js,AttachmentDownload.js

---
+++ src/routes/Attachment/AttachmentDownload.js
@@ -0,0 +1,43 @@
+import React, { PureComponent} from 'react';
+import { connect } from 'dva';
+import {  Card, Form, List, Divider } from 'antd';
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+
+import styles from './style.less';
+
+@connect(({ absAttachment}) => ({
+  absAttachment,
+}))
+export default class AttachmentDownload extends PureComponent {
+  componentWillReceiveProps() {
+  //componentDidMount() {
+    const { dispatch } = this.props;
+    let attachmentIdList = this.props.attachmentIdList;
+    //let attachmentIdList = ['123','234'];
+    console.log('xxxxxxxxxxxx1:'+JSON.stringify(attachmentIdList));
+    dispatch({
+      type: 'absAttachment/queryAttachmentListByIdList',
+      payload: JSON.stringify(attachmentIdList),
+    });
+  }
+
+  render() {
+    const { absAttachment: { data }} = this.props;
+    return (
+    <Card bordered={false} title="附件列表:">
+      <div className={styles.tableList}>
+        <List
+          dataSource={data}
+          renderItem={item => (
+            <List.Item >
+              <List.Item.Meta
+                title={<a href={"http://localhost:8080"+item.filePath}>{item.fileName}</a>}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
+    </Card>
+    );
+  }
+}
---
+++ src/routes/Attachment/AttachmentUpload.js
@@ -0,0 +1,88 @@
+import React, { PureComponent } from 'react';
+import {
+  Card, Upload, message, Button, Icon,
+} from 'antd';
+import { connect } from 'dva';
+
+@connect(({ absAttachment,loading}) => ({
+  absAttachment,
+  uploading : loading.effects['absAttachment/attachmentUpload'],
+  }))
+export default class AttachmentUpload extends PureComponent {
+  state = {
+    fileList: [],
+    attachmentIdList: [],
+  }
+  handleChange = (info) => {
+    let fileList = info.fileList;
+
+    // 1. Limit the number of uploaded files
+    //    Only to show two recent uploaded files, and old ones will be replaced by the new
+    fileList = fileList.slice(-2);
+
+    // 2. read from response and show file link
+    fileList = fileList.map((file) => {
+      if (file.response) {
+        // Component will show file.url as link
+        file.url = 'http://localhost:8080'+file.response.file_path;
+      }
+      return file;
+    });
+
+    // 3. filter successfully uploaded files according to response from server
+    fileList = fileList.filter((file) => {
+      if (file.response) {
+        return file.response.status === 'success';
+      }
+      return true;
+    });
+
+    this.setState({ fileList:fileList });
+
+    if (info.file.status === 'done') {
+        message.success(`${info.file.name} attachment uploaded successfully`);
+
+        this.props.dispatch({
+            type: 'absAttachment/attachmentUpload',
+            payload: {
+              fileName: info.file.response.file_name,
+              filePath: info.file.response.file_path,
+              fileSha1: info.file.response.file_sha1,
+            },
+            // 增加一个回调，便于在请求完成后执行一些页面的控制逻辑
+            callback: (attachmentId) => {
+              let attachmentIdList = this.state.attachmentIdList;
+              attachmentIdList.push(attachmentId);
+              this.setState({attachmentIdList:attachmentIdList});
+              //这里将子组件的信息传递给了父组件  
+              this.props.callbackParent(this.state.attachmentIdList);  
+            },
+        });
+
+        console.log(info.file.response.file_name);
+        console.log(info.file.response.file_path);
+        console.log(info.file.response.file_sha1);
+
+    } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} attachment upload failed.`);
+    }
+
+  }
+  render() {
+    const props = {
+      name: 'file',
+      action: '/upload',
+      onChange: this.handleChange,
+      multiple: true,
+    };
+    const {uploading} = this.props;
+
+    return (
+      <Upload {...props} fileList={this.state.fileList}>
+        <Button> 
+          <Icon type="upload" loading={uploading}/> upload
+        </Button>
+      </Upload>
+    );
+  }
+}
